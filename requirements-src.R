50b83ea7ad00cb1bb977ccb7e43f724feb45497f
AhmedElgahama <40144555+AhmedElgahama@users.noreply.github.com>
2022-12-05 22:26:51 +0200

    solving error
    Can't use NA as column index with `[` at positions 4

--- requirements-src.R
+++ requirements-src.R
@@ -1,5 +1,8 @@
 pkgs <- c(
-    'tictoc', 'imputeTS', 'pROC', 'caTools', 'CatEncoders', 'Metrics','stats'
+    'tictoc', 'imputeTS', 'pROC', 'caTools', 'CatEncoders', 'Metrics'
 )
 
 install.packages(pkgs)
+
+
+install.packages("stats", version='3.6.1')
